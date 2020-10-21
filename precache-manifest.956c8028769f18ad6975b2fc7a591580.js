5589a171f7a9c39625b1eaace64038549443da7d
Kamran Elchuzade <abbox@Kamrans-iMac.local>
2020-10-22 00:05:58 +0300

    Updates

---
+++ precache-manifest.956c8028769f18ad6975b2fc7a591580.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70195ded5daf2422ed5c2d774a7c74a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "88f9075b9b14a0a7ad44",
+    "url": "./static/css/main.c3a9bed8.chunk.css"
+  },
+  {
+    "revision": "f86857ff0f0958179d8b",
+    "url": "./static/js/2.18e3508f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18e3508f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f9075b9b14a0a7ad44",
+    "url": "./static/js/main.7584091c.chunk.js"
+  },
+  {
+    "revision": "5aece4a66d31115dca5e",
+    "url": "./static/js/runtime-main.ae59c665.js"
+  }
+]);
